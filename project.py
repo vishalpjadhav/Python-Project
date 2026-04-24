4088abcd3012f77443029c3308cec308d87c8f3a
vishalpjadhav <vishaaljadhav11@gmail.com>
2026-04-24 17:14:36 +0530

    Number Guessing Game Using Python

--- project.py
+++ project.py
@@ -1,4 +1,18 @@
-import numpy as np 
-arr = np.array([10,20,30])
-
-print(arr * 10)
+import random
+target = random.randint(1,100)
+while True :
+    user_choice = input("Guess The Number Or Quit (Q) :- ").upper()
+    if(user_choice == "Q"):
+        break
+    
+    user_choice = int(user_choice)
+    if(user_choice == target):
+        print("Success : Correct Guess ")
+        break
+    elif (user_choice < target):
+        print("Your Choice is less than target Take Bigger Guess")
+        
+    else :
+        print("Your Choice is Too Big Take Smaller Guess")
+        
+print("------Game Over ------")
